4ea9b2b5612cc7efb68cc17c7b309de3f1626cd8
ryanchou-dev <ryanchou111@gmail.com>
2023-03-18 23:19:17 -0700

    in order to fix bounding-box issues, you have to tinker with some files from .prismo/static/common (remove absolute positioning of outer div)

--- content.tsx
+++ content.tsx
@@ -1,5 +1,7 @@
 import cssText from "data-text:~style.css"
 import { useEffect, useState } from "react"
+import { render } from "react-dom"
+import Draggable, { DraggableCore } from "react-draggable"
 import { io } from "socket.io-client"
 
 import { getAvatar } from "~util/getAvatar"
@@ -18,6 +20,7 @@ const CustomButton = () => {
   const [urlsMap, setUrlsMap] = useState({})
   const [usernameMap, setUsernameMap] = useState({})
   const [avatarMap, setAvatarMap] = useState({})
+
   useEffect(() => {
     ;(async () => {
       // check if the user has an uid already
@@ -78,30 +81,44 @@ const CustomButton = () => {
       })
     })()
   }, [])
+  //   <div className="box no-cursor">
+  //     <strong className="cursor"><div>Drag here</div></strong>
+  //     <div>You must click my handle to drag me</div>
+  //   </div>
+  // </Draggable>
   return (
-    <div className="px-4 py-4 text-black">
-      <div className="bg-gray-200 bg-opacity-70 backdrop-blur-sm hover:bg-opacity-100 flex px-2 py-2 rounded-md flex-col gap-y-4">
-        {Object.keys(urlsMap).map((uid) => (
-          <div key={uid} className="flex items-center gap-4">
-            <img className="w-10 h-10" src={avatarMap[uid]} />
-            <div className="max-w-md">
-              <div className="truncate text-lg font-semibold leading-tight">
-                {uid in usernameMap ? usernameMap[uid] : uid}
-              </div>
-              <div className="flex gap-1.5 items-center">
-                <img
-                  className="w-6 h-6 bg-white rounded px-0.5 py-0.5"
-                  src={`https://www.google.com/s2/favicons?domain=${urlsMap[uid]}&sz=32`}
-                />
-                <div className="text-gray-800 font-light truncate text-sm">
-                  {urlsMap[uid]}
+    <Draggable handle=".handle" enableUserSelectHack={false}>
+      <div className="px-4 py-4 text-black">
+        <div className="bg-gray-200 bg-opacity-70 backdrop-blur-sm hover:bg-opacity-100 flex px-2 py-2 rounded-md flex-col gap-y-4">
+          {Object.keys(urlsMap).map((uid) => (
+            <div key={uid} className="flex items-center gap-4">
+              <img className="w-10 h-10" src={avatarMap[uid]} />
+              <div className="max-w-md">
+                <div className="truncate text-lg font-semibold leading-tight">
+                  {uid in usernameMap ? usernameMap[uid] : uid}
+                </div>
+                <div className="flex gap-1.5 items-center">
+                  <img
+                    className="w-6 h-6 bg-white rounded px-0.5 py-0.5"
+                    src={`https://www.google.com/s2/favicons?domain=${urlsMap[uid]}&sz=32`}
+                  />
+                  <div className="text-gray-800 font-light truncate text-sm">
+                    {urlsMap[uid]}
+                  </div>
                 </div>
               </div>
             </div>
+          ))}
+          <div
+            onMouseDown={(e) => e.preventDefault()}
+            className="hover:cursor-move bg-blue-500 top-0 right-0 handle">
+            move me
           </div>
-        ))}
+        </div>
       </div>
-    </div>
+    </Draggable>
+    // <Draggable>
+    // </Draggable>
   )
 }
 
