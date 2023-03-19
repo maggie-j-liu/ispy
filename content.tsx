3363c2c5004335456465f6b6a17152ea4f219223
Maggie Liu <maggie.j.liu@gmail.com>
2023-03-18 21:19:32 -0700

    style overlay

--- content.tsx
+++ content.tsx
@@ -1,3 +1,4 @@
+import cssText from "data-text:~style.css"
 import { useEffect, useState } from "react"
 import { io } from "socket.io-client"
 
@@ -5,6 +6,12 @@ import { getAvatar } from "~util/getAvatar"
 import { getRoomId } from "~util/getRoomId"
 import { getUid } from "~util/getUid"
 import { getUsername } from "~util/getUsername"
+
+export const getStyle = () => {
+  const style = document.createElement("style")
+  style.textContent = cssText
+  return style
+}
 
 const CustomButton = () => {
   const [message, setMessage] = useState("")
@@ -72,12 +79,28 @@ const CustomButton = () => {
     })()
   }, [])
   return (
-    <div style={{ display: "flex", flexDirection: "column" }}>
-      {Object.keys(urlsMap).map((uid) => (
-        <div key={uid}>
-          {uid in usernameMap ? usernameMap[uid] : uid}: {urlsMap[uid]}
-        </div>
-      ))}
+    <div className="px-4 py-4 text-black">
+      <div className="bg-gray-200 bg-opacity-70 backdrop-blur-sm hover:bg-opacity-100 flex px-2 py-2 rounded-md flex-col gap-y-4">
+        {Object.keys(urlsMap).map((uid) => (
+          <div key={uid} className="flex items-center gap-4">
+            <img className="w-10 h-10" src={avatarMap[uid]} />
+            <div className="max-w-md">
+              <div className="truncate text-lg font-semibold leading-tight">
+                {uid in usernameMap ? usernameMap[uid] : uid}
+              </div>
+              <div className="flex gap-1.5 items-center">
+                <img
+                  className="w-6 h-6 bg-white rounded px-0.5 py-0.5"
+                  src={`https://www.google.com/s2/favicons?domain=${urlsMap[uid]}&sz=32`}
+                />
+                <div className="text-gray-800 font-light truncate text-sm">
+                  {urlsMap[uid]}
+                </div>
+              </div>
+            </div>
+          </div>
+        ))}
+      </div>
     </div>
   )
 }
