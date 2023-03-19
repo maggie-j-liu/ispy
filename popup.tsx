db0ce3bc84e45b84f1bacf8e3390d37c3772510d
ryanchou-dev <ryanchou111@gmail.com>
2023-03-18 19:22:24 -0700

    format and rename

--- popup.tsx
+++ popup.tsx
@@ -54,7 +54,7 @@ const Popup = () => {
     setEnteredCode("")
   }
 
-  const doNameStuff = async (newName: string) => {
+  const changeName = async (newName: string) => {
     setName(newName)
     await chrome.storage.sync.set({ name: newName })
   }
@@ -70,19 +70,14 @@ const Popup = () => {
   if (loading) {
     return null
   }
+
   return (
     <div className="min-w-[20rem] py-12 px-8 text-base">
       <p className="font-mono text-center text-2xl">
         i<span className="font-bold text-3xl">Spy</span>
       </p>
-      <div>ur name:</div>
-      <input
-        className="px-2 py-1 ring-blue-300 ring-2 rounded-md w-full bg-gray-100"
-        id="joincode"
-        type="text"
-        value={name}
-        onChange={(e) => doNameStuff(e.target.value)}
-      />
+      <br />
+
       <div>Current room (share this with friends!)</div>
       <pre>{roomId}</pre>
       <button
@@ -107,6 +102,19 @@ const Popup = () => {
           onClick={() => joinRoom()}>
           Join Room
         </button>
+      </div>
+      <br />
+      <div>
+        <label htmlFor="namefield" className="pt-12 inline">
+          Username:
+        </label>
+        <input
+          className="inline px-2 py-1 ring-blue-300 ring-2 rounded-md w-full bg-gray-100"
+          id="namefield"
+          type="text"
+          value={name}
+          onChange={(e) => changeName(e.target.value)}
+        />
       </div>
     </div>
   )
